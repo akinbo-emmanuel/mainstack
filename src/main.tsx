443289010c277abe5ad3159251e1f925c3734a22
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2025-11-03 10:30:14 +0100

    Vercel Analytiucs enabled

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,14 @@ import App from "./App.tsx";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient.ts";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+
+      <Analytics />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
